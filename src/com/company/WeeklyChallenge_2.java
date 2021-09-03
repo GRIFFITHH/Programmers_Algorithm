630f1015ee22dfc5bf71eac5d722dc14927c1689
GRIFFITHH <glqglq032@gmail.com>
2021-09-03 15:19:33 +0900

    9월2일 commit
    Programmers WeeklyChallenge_2
    
    제일 중요한 조건을 빼먹었음 , 동일한 최고점 최저점이 존재할 시 삭제하지않고 그대로 연산해야함

--- src/com/company/WeeklyChallenge_2.java
+++ src/com/company/WeeklyChallenge_2.java
@@ -2,61 +2,74 @@ package com.company;
 
 public class WeeklyChallenge_2 {
     public String solution(int[][] scores) {
-        String answer = "1";
+        StringBuilder sb = new StringBuilder("");
 
         for (int i = 0; i < scores.length; i++) {
             int sum = 0;
             int max = 0;
             int min = 100;
-            int aver = 0;
+            float aver = 0;
 
             for (int j = 0; j < scores[i].length; j++) {
-                max = Math.max(scores[i][j], max); //최고점수
-                min = Math.min(scores[i][j], min); //최저점수
-                sum += scores[i][j];
+                max = Math.max(scores[j][i], max); //최고점수
+                min = Math.min(scores[j][i], min); //최저점수
+                sum += scores[j][i];
                 }
 //            System.out.println("Before ="+sum); //최고점수 , 최저점수 빼기전test
-            if (( min==scores[i][i] || max==scores[i][i] )) {
+
+            if ((min == scores[i][i] || max == scores[i][i])) {//자기자신에게 준 점수가 최대점수 or 최소점수일경우 종합에서 제외
                 sum -= scores[i][i];
-
-            }//자기자신에게 준 점수가 최대점수 or 최소점수일경우 종합에서 제외
-            aver = sum / scores.length;
-            System.out.println(aver); // 평균 구하는것까진 완료.. String을 배열로이용해서 학점을 출력해야함..
-
-            if (sum / scores[i].length >= 90) {
-                System.out.println("1");
-               answer = answer+"A";
-            } else if (sum / scores[i].length < 90 && sum / scores[i].length >= 80) {
-                answer = answer+"B";
-            } else if (sum / scores[i].length < 80 && sum / scores[i].length >= 70) {
-                answer = answer+"C";
-            } else if (sum / scores[i].length < 70 && sum / scores[i].length >= 50) {
-                answer = answer+"D";
+                aver = sum / (float)(scores.length - 1);
             } else {
-                answer = answer+"F";
+                aver = sum / (float)(scores.length);
             }
 
 
+            System.out.println(aver);
+            //System.out.println(aver); // 평균 구하는것까진 완료.. String을 배열로이용해서 학점을 출력해야함..
+            if (aver >= 90) {
+                sb.insert(i, "A");
+            } else if (aver < 90 && aver >= 80) {
+                sb.insert(i, "B");
+            } else if (aver < 80 && aver >= 70) {
+                sb.insert(i, "C");
+            } else if (aver < 70 && aver >= 50) {
+                sb.insert(i , "D");
+            } else {
+                sb.insert(i, "F");
+            }
 //            System.out.println("After = "+sum); // 검증후 최대점수 , 최소점수 제거test
 //            System.out.println("Max = "+max);
 //            System.out.println("Min = "+min);
         }
+
 //        System.out.println("scores[0][0] = " + scores[0][0]);
 //        System.out.println("scores[1][1] = " + scores[1][1]);
 //        System.out.println("scores[2][2] = " + scores[2][2]);
 //        System.out.println("scores[3][3] = " + scores[3][3]);
 //        System.out.println("scores[4][4] = " + scores[4][4]);
-        return answer;
+        return sb.toString();
     }
 
     public void test() {
-        int[][] scores = {{10,20,30,40,50},
-                          {50,50,40,60,70},
-                          {60,30,40,20,67},
-                          {70,60,50,40,20},
-                          {10,20,40,50,20}};
+        int[][] scores = {{10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,60,60,40,30,40,30,},};
 
-        solution(scores);
+        int[][] scores2= {{0,90,98,88,65},
+                          {0,40,99,85,77},
+                          {0,88,95,80,67},
+                          {0,57,100,80,65},
+                          {0,90,94,75,65}};
 
+        String result = solution(scores2);
+        System.out.println();
+        System.out.print(result);
     }
 }
