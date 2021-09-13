11a713ba6d359806b336466e57a9d850dd209b4a
GRIFFITHH <glqglq032@gmail.com>
2021-09-13 17:13:49 +0900

    9월14일 commit
    Programmers WeeklyChallenge_2
    
    Complete~

---
+++ src/com/company/WeeklyChallengeAnswer_2.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class WeeklyChallengeAnswer_2 {
+    public String main(int[][] scores) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<scores.length; i++) {
+            int max = 0;
+            int min = 101;
+            int sum = 0;
+            int divide = scores.length;
+            for(int j=0; j<scores.length; j++) {
+                int score = scores[j][i];
+                if(i != j) {
+                    if(score < min) {
+                        min = score;
+                    }
+                    if(score > max) {
+                        max = score;
+                    }
+                }
+                sum += score;
+            }
+            if(scores[i][i] < min || scores[i][i] > max) {
+                sum -= scores[i][i];
+                divide--;
+            }
+            double score = (double) sum / divide;
+            builder.append(score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 50 ? "D" : "F" );
+        }
+        return builder.toString();
+    }
+}
--- src/com/company/WeeklyChallenge_2.java
+++ src/com/company/WeeklyChallenge_2.java
@@ -2,73 +2,83 @@ package com.company;
 
 public class WeeklyChallenge_2 {
     public String solution(int[][] scores) {
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < scores.length; i++) {
             int sum = 0;
             int max = 0;
             int min = 100;
-            float aver = 0;
+            float aver;
 
             for (int j = 0; j < scores[i].length; j++) {
-                max = Math.max(scores[j][i], max); //최고점수
-                min = Math.min(scores[j][i], min); //최저점수
+                max = max(scores[j][i], max);
+                min = min(scores[j][i], min);
                 sum += scores[j][i];
                 }
-//            System.out.println("Before ="+sum); //최고점수 , 최저점수 빼기전test
 
-            if ((min == scores[i][i] || max == scores[i][i])) {//자기자신에게 준 점수가 최대점수 or 최소점수일경우 종합에서 제외
+
+            if ((min == scores[i][i] || max == scores[i][i])) {
                 sum -= scores[i][i];
                 aver = sum / (float)(scores.length - 1);
             } else {
                 aver = sum / (float)(scores.length);
             }
 
-
-            System.out.println(aver);
-            //System.out.println(aver); // 평균 구하는것까진 완료.. String을 배열로이용해서 학점을 출력해야함..
-            if (aver >= 90) {
-                sb.insert(i, "A");
-            } else if (aver < 90 && aver >= 80) {
-                sb.insert(i, "B");
-            } else if (aver < 80 && aver >= 70) {
-                sb.insert(i, "C");
-            } else if (aver < 70 && aver >= 50) {
-                sb.insert(i , "D");
-            } else {
-                sb.insert(i, "F");
-            }
-//            System.out.println("After = "+sum); // 검증후 최대점수 , 최소점수 제거test
-//            System.out.println("Max = "+max);
-//            System.out.println("Min = "+min);
+            getScore(sb, i, aver);
         }
 
-//        System.out.println("scores[0][0] = " + scores[0][0]);
-//        System.out.println("scores[1][1] = " + scores[1][1]);
-//        System.out.println("scores[2][2] = " + scores[2][2]);
-//        System.out.println("scores[3][3] = " + scores[3][3]);
-//        System.out.println("scores[4][4] = " + scores[4][4]);
+
         return sb.toString();
+    }
+
+    private void getScore(StringBuilder sb, int i, float aver) {
+        if (aver >= 90) {
+            sb.insert(i, "A");
+        } else if (aver < 90 && aver >= 80) {
+            sb.insert(i, "B");
+        } else if (aver < 80 && aver >= 70) {
+            sb.insert(i, "C");
+        } else if (aver < 70 && aver >= 50) {
+            sb.insert(i, "D");
+        } else {
+            sb.insert(i, "F");
+        }
+    }
+
+    public int max(int a, int b) {
+        if (a == b) {
+            return 101;
+        }
+        return Math.max(a, b);
+    }
+
+    public int min(int a, int b) {
+        if (a == b) {
+            return -1; 
+            //중복되는 최솟값, 최댓값이 존재할경우 1~100 범위를 벗어난 최댓값,최솟값을 설정해서 어느값하나 제외하지않고 정상적으로 연산시킴 
+            //내가준 점수가 최솟값인데 그 점수랑 겹치는 값이 있으면 정상적으로 계산 , 그외에도 값이 겹치더라도 어쨌든 정상적으로 연산진행
+        }
+        return Math.min(a, b);
     }
 
     public void test() {
         int[][] scores = {{10,20,30,60,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
+                            {10,20,30,30,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
                             {10,20,30,60,60,40,30,40,30,},
-                            {10,20,30,60,60,40,30,40,30,},
-                            {10,20,30,60,60,40,30,40,30,},};
+                            {10,20,30,60,60,40,30,40,30,}};
 
-        int[][] scores2= {{0,90,98,88,65},
-                          {0,40,99,85,77},
-                          {0,88,95,80,67},
-                          {0,57,100,80,65},
-                          {0,90,94,75,65}};
+        int[][] scores2= {{0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0},
+                          {0,0,0,0,0}};
 
-        String result = solution(scores2);
+        String result = solution(scores);
         System.out.println();
         System.out.print(result);
     }
