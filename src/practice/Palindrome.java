96fd1b7951f0af3442bc389e70c62aae7c8038cd
GRIFFITHH <glqglq032@gmail.com>
2021-09-20 23:22:01 +0900

    9월20일 commit
    Palindrome 공부

---
+++ src/practice/Palindrome.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("asg");
+        String string = sb.reverse().toString();
+        System.out.println(string);
+    }
+}
